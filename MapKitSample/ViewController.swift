5ef23f2241eab8cdd6705ec5aa732ee035500da4
EricaFujita <46892139+EricaFujita@users.noreply.github.com>
2020-07-31 10:02:39 +0900

    Initial Commit

---
+++ MapKitSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapKitSample
+//
+//  Created by 藤田えりか on 2020/07/31.
+//  Copyright © 2020 com.erica. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
